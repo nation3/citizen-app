37d0032ab2c4f851441cde3d0721e7c49de5bdcc
Luis Cuende <me@luisivan.net>
2022-03-24 13:02:14 +0000

    Cleaned some code

--- ui/lib/use-wagmi.js
+++ ui/lib/use-wagmi.js
@@ -38,28 +38,20 @@ function _useContractWrite(config, method, argsAndOverrides) {
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
 
-  const [
-    { data: signerData, error: signerError, loading: signerLoading },
-    getSigner,
-  ] = useSigner()
+  const [{ data: signerData, error: signerError, loading: signerLoading }] =
+    useSigner()
   const contract = useContract({
     ...config,
     signerOrProvider: signerData,
   })
 
   const write = useCallback(async () => {
-    const signer = await getSigner()
-    contract.connect(signer)
-    console.log(contract)
     try {
       setLoading(true)
       let data
       if (argsAndOverrides) {
-        console.log('contract')
-        contract.connect(signer)
         data = await contract[method](...argsAndOverrides.args)
       } else {
-        console.log(contract)
         data = await contract[method]()
       }
       setData(data)
@@ -74,76 +66,7 @@ function _useContractWrite(config, method, argsAndOverrides) {
       console.error(error)
       errorContext.addError([error])
     }
-  }, [config, signerData, argsAndOverrides])
+  }, [config, signerData, signerLoading, argsAndOverrides])
   errorContext.addError([signerError])
   return [{ data, error, loading }, write]
-}
-
-function _useContractWrite2(contractConfig, functionName, argsAndOverrides) {
-  /*const {
-    state: { connector },
-  } = useContext()*/
-  const [
-    { data: signerData, error: signerError, loading: signerLoading },
-    getSigner,
-  ] = useSigner({ skip: true })
-
-  const contract = useContract(contractConfig)
-  const [state, setState] = useState({ loading: false })
-
-  //const cancelQuery = useCancel()
-  const write = useCallback(
-    async (config) => {
-      /*let didCancel = false
-      cancelQuery(() => {
-        didCancel = true
-      })*/
-
-      try {
-        // const config_ = config ?? { args, overrides }
-        //if (!connector) throw new ConnectorNotFoundError()
-        /*const params = [
-          ...(Array.isArray(config_.args)
-            ? config_.args
-            : config_.args
-            ? [config_.args]
-            : []),
-          ...(config_.overrides ? [config_.overrides] : []),
-        ]*/
-        const params = argsAndOverrides.args
-
-        setState((x) => ({
-          ...x,
-          error: undefined,
-          loading: true,
-          response: undefined,
-        }))
-        const signer = await getSigner()
-        const contract_ = contract.connect(signer)
-        const response = await contract_[functionName](...params)
-        /*if (!didCancel) {
-          setState((x) => ({ ...x, loading: false, response }))
-        }*/
-        return { data: response, error: undefined }
-      } catch (error_) {
-        let error = error_
-        if (error_.code === 4001) console.log('UserRejectedRequestError')
-        /*if (!didCancel) {
-          setState((x) => ({ ...x, error, loading: false }))
-        }*/
-        console.log(error)
-        return { data: undefined, error }
-      }
-    },
-    [argsAndOverrides, signerData, contract, functionName]
-  )
-
-  return [
-    {
-      data: state.response,
-      error: state.error,
-      loading: state.loading,
-    },
-    write,
-  ]
 }
