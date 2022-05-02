8ae408177feeb3c6fdfccca2f3485b1ecd27347b
Luis Cuende <me@luisivan.net>
2022-05-02 14:26:33 +0100

    fix(ui): Set social description

--- ui/components/Head.js
+++ ui/components/Head.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 
-export default function WebsiteHead({ title, description, image, type }) {
+export default function WebsiteHead({ title }) {
+  const description = 'Citizen app for the citizens of Nation3'
+  const image = 'https://nation3.org/social.jpg'
+
   return (
     <Head>
       <title>Nation3 | {title}</title>
@@ -8,10 +11,10 @@ export default function WebsiteHead({ title, description, image, type }) {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
-      <meta property="og:type" content={type} />
+      <meta property="og:type" content="website" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="https://nation3.org/social.jpg" />
+      <meta name="twitter:image" content={image} />
       <meta name="twitter:card" content="summary" />
     </Head>
   )
