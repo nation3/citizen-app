2a96a631c029addd49ee83f4da9223a0dcc9aec0
gregfromstl <gregfromstl@gmail.com>
2022-05-17 14:39:39 -0500

    remove log

--- ui/lib/liquidity-rewards.js
+++ ui/lib/liquidity-rewards.js
@@ -29,7 +29,6 @@ export function useLiquidityRewards({ nationPrice, poolValue, address }) {
       throwOnRevert: false, // assumes a reverted transaction means no claimable rewards
       skip: !address,
     })
-  console.log(unclaimedRewards)
 
   const { data: userDeposit, isLoading: userDepositLoading } = useContractRead(
     contractParams,
