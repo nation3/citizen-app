34e429915082419b711f66fb7b35f2b7b110625f
gregfromstl <gregfromstl@gmail.com>
2022-05-17 14:30:09 -0500

    import networkToId

--- ui/components/ErrorProvider.js
+++ ui/components/ErrorProvider.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react'
 import { useNetwork } from 'wagmi'
+import networkToId from '../lib/networkToId'
 
 const ErrorContext = createContext([])
 
