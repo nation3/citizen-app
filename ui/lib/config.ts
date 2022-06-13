0d698eae8781edc8f67db857fbd0a509186e4044
karming.wan <karming1997@gmail.com>
2022-06-13 21:11:55 +0800

    revamp configuration

--- ui/lib/config.ts
+++ ui/lib/config.ts
@@ -1,45 +1,46 @@
-let config = {
-  nationToken: process.env.NEXT_PUBLIC_NATION_ADDRESS,
-  veNationToken: process.env.NEXT_PUBLIC_VENATION_ADDRESS,
-  veNationRequiredStake: process.env.NEXT_PUBLIC_VENATION_REQUIRED_STAKE,
-  veNationRewardsMultiplier:
-    process.env.NEXT_PUBLIC_VENATION_REWARDS_MULTIPLIER,
-  balancerVault: process.env.NEXT_PUBLIC_BALANCER_VAULT_ADDRESS,
-  balancerPoolId: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_ID,
-  balancerLPToken: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_TOKEN,
-  lpRewardsContract: process.env.NEXT_PUBLIC_LP_REWARDS_CONTRACT_ADDRESS,
-  nationPassportNFT: process.env.NEXT_PUBLIC_PASSPORT_NFT_ADDRESS,
-  nationPassportNFTIssuer: process.env.NEXT_PUBLIC_PASSPORT_NFT_ISSUER_ADDRESS,
-  nationDropContracts: [
-    process.env.NEXT_PUBLIC_NATION_DISTRIBUTOR1_CONTRACT_ADDRESS,
-    process.env.NEXT_PUBLIC_NATION_DISTRIBUTOR2_CONTRACT_ADDRESS,
-  ],
-  nationDropAmount: 1,
+const zeroAddress = '0x0000000000000000000000000000000000000000'
+
+interface DeploymentConfig {
+  nationToken: string;
+  veNationToken: string;
+  nationDropContracts: string[],
+  balancerLPToken: string,
+  lpRewardsContract: string,
+  nationPassportNFT: string,
+  nationPassportNFTIssuer: string
+}
+interface Config {
+  nationToken: string,
+  veNationToken: string,
+  veNationRequiredStake: number,
+  veNationRewardsMultiplier: number,
+  balancerVault: string,
+  balancerPoolId: string,
+  balancerLPToken: string,
+  lpRewardsContract: string,
+  nationPassportNFT: string,
+  nationPassportNFTIssuer: string,
+  nationDropContracts: string[],
+  nationDropAmount: number,
 }
 
-if (process.env.NEXT_PUBLIC_CHAIN !== 'mainnet') {
-  const zeroAddress = '0x0000000000000000000000000000000000000000'
-  const devDeployments = require(`../../contracts/deployments/${process.env.NEXT_PUBLIC_CHAIN}.json`)
-  config = {
-    nationToken: devDeployments.nationToken || zeroAddress,
-    veNationToken: devDeployments.veNationToken || zeroAddress,
-    // @ts-expect-error ts-migrate(2322) FIXME: Type 'number' is not assignable to type 'string | ... Remove this comment to see the full error message
-    veNationRequiredStake: 2,
-    // @ts-expect-error ts-migrate(2322) FIXME: Type 'number' is not assignable to type 'string | ... Remove this comment to see the full error message
-    veNationRewardsMultiplier: 2.5,
-    balancerVault: process.env.NEXT_PUBLIC_BALANCER_VAULT_ADDRESS,
-    balancerPoolId: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_ID,
-    balancerLPToken: devDeployments.balancerLPToken || zeroAddress,
-    lpRewardsContract: devDeployments.lpRewardsContract || zeroAddress,
-    nationPassportNFT: devDeployments.nationPassportNFT || zeroAddress,
-    nationPassportNFTIssuer:
-      devDeployments.nationPassportNFTIssuer || zeroAddress,
-    nationDropContracts: devDeployments.nationDropContracts || [zeroAddress],
-    nationDropAmount: 1,
-  }
+const chain = process.env.NEXT_PUBLIC_CHAIN || "goerli";
+const defaultConfig = require(`../../contracts/deployments/${chain}.json`) as DeploymentConfig
+const config: Config = {
+  nationToken: process.env.NEXT_PUBLIC_NATION_ADDRESS || zeroAddress,
+  veNationToken: process.env.NEXT_PUBLIC_VENATION_ADDRESS || zeroAddress,
+  veNationRequiredStake: Number(process.env.NEXT_PUBLIC_VENATION_REQUIRED_STAKE) || 2,
+  veNationRewardsMultiplier: Number(process.env.NEXT_PUBLIC_VENATION_REWARDS_MULTIPLIER) || 2.5,
+  balancerVault: process.env.NEXT_PUBLIC_BALANCER_VAULT_ADDRESS || zeroAddress,
+  balancerPoolId: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_ID || zeroAddress,
+  balancerLPToken: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_TOKEN || defaultConfig.balancerLPToken || zeroAddress,
+  lpRewardsContract: process.env.NEXT_PUBLIC_LP_REWARDS_CONTRACT_ADDRESS || defaultConfig.lpRewardsContract || zeroAddress,
+  nationPassportNFT: process.env.NEXT_PUBLIC_PASSPORT_NFT_ADDRESS || defaultConfig.nationPassportNFT || zeroAddress,
+  nationPassportNFTIssuer: process.env.NEXT_PUBLIC_PASSPORT_NFT_ISSUER_ADDRESS || defaultConfig.nationPassportNFTIssuer || zeroAddress,
+  nationDropContracts: JSON.parse(process.env.NEXT_PUBLIC_NATION_DISTRIBUTOR_CONTRACT_ADDRESS || "null") || defaultConfig.nationDropContracts || [zeroAddress],
+  nationDropAmount: Number(process.env.NEXT_NATION_DROP_AMOUNT) || 1
 }
 
-console.log(process.env.NEXT_PUBLIC_CHAIN)
 console.log(config)
 
 export const {
