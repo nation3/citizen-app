0241a5b78702ebc4071be9a1be9b3d41560c04f7
xPi2 <uxiopineiro@gmail.com>
2022-06-27 19:34:47 +0200

    fix: Missing default values on config parsing

--- ui/lib/config.ts
+++ ui/lib/config.ts
@@ -1,14 +1,17 @@
 const zeroAddress = '0x0000000000000000000000000000000000000000'
 
 interface DeploymentConfig {
-  nationToken: string;
-  veNationToken: string;
+  nationToken: string,
+  veNationToken: string,
   nationDropContracts: string[],
+  balancerVault: string,
+  balancerPoolId: string,
   balancerLPToken: string,
   lpRewardsContract: string,
   nationPassportNFT: string,
   nationPassportNFTIssuer: string
 }
+
 interface Config {
   nationToken: string,
   veNationToken: string,
@@ -27,12 +30,12 @@ interface Config {
 const chain = process.env.NEXT_PUBLIC_CHAIN || "goerli";
 const defaultConfig = require(`../../contracts/deployments/${chain}.json`) as DeploymentConfig
 const config: Config = {
-  nationToken: process.env.NEXT_PUBLIC_NATION_ADDRESS || zeroAddress,
-  veNationToken: process.env.NEXT_PUBLIC_VENATION_ADDRESS || zeroAddress,
+  nationToken: process.env.NEXT_PUBLIC_NATION_ADDRESS || defaultConfig.nationToken || zeroAddress,
+  veNationToken: process.env.NEXT_PUBLIC_VENATION_ADDRESS || defaultConfig.veNationToken || zeroAddress,
   veNationRequiredStake: Number(process.env.NEXT_PUBLIC_VENATION_REQUIRED_STAKE) || 2,
   veNationRewardsMultiplier: Number(process.env.NEXT_PUBLIC_VENATION_REWARDS_MULTIPLIER) || 2.5,
-  balancerVault: process.env.NEXT_PUBLIC_BALANCER_VAULT_ADDRESS || zeroAddress,
-  balancerPoolId: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_ID || zeroAddress,
+  balancerVault: process.env.NEXT_PUBLIC_BALANCER_VAULT_ADDRESS || defaultConfig.balancerVault || zeroAddress,
+  balancerPoolId: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_ID || defaultConfig.balancerPoolId || zeroAddress,
   balancerLPToken: process.env.NEXT_PUBLIC_BALANCER_NATION_ETH_POOL_TOKEN || defaultConfig.balancerLPToken || zeroAddress,
   lpRewardsContract: process.env.NEXT_PUBLIC_LP_REWARDS_CONTRACT_ADDRESS || defaultConfig.lpRewardsContract || zeroAddress,
   nationPassportNFT: process.env.NEXT_PUBLIC_PASSPORT_NFT_ADDRESS || defaultConfig.nationPassportNFT || zeroAddress,
