9ab8da940b838805eb6c0370dc4bf55a6e5ae45f
Luis Cuende <me@luisivan.net>
2022-07-20 12:02:56 +0100

    feat(ui): Redesigned layoust

--- ui/pages/citizen.tsx
+++ ui/pages/citizen.tsx
@@ -53,95 +53,91 @@ export default function Citizen() {
         <Confetti key={number.toString()} />
       ))}
       <Head title="Welcome, citizen" />
-      <MainCard
-        title="Welcome, citizen"
-        loading={!account || !passportData?.id || !passportData?.nft}
-        maxWidthClassNames="max-w-md md:max-w-3xl"
-      >
-        <div className="flex flex-col md:flex-row justify-between">
-          <div
-            style={{
-              width: window.innerWidth > 420 ? '390px' : '280px',
-            }}
-          >
-            <Card
-              style={{
-                width: window.innerWidth > 420 ? '390px' : '280px',
-                height: window.innerWidth > 420 ? '450px' : '320px',
-                cursor: 'pointer',
-                position: 'relative',
-              }}
-              onClick={() => addConfetti()}
-            >
-              {passportData?.nft?.image && (
-                <Image src={passportData.nft.image} layout="fill" />
-              )}
-            </Card>
+      <div className="w-full h-full max-w-5xl m-auto flex flex-row items-center">
+        <div className="pt-64 pb-24 md:pt-4 lg:pb-2 overflow-auto">
+          <h2 className="text-center md:text-left text-3xl font-medium mx-4 lg:mx-32 mb-8">
+            Welcome, citizen
+          </h2>
+          <div className="flex flex-col md:flex-row justify-between items-center gap-8 px-4 lg:px-0 lg:mx-32">
+            <div>
+              <Card
+                style={{
+                  width: window.innerWidth > 420 ? '390px' : '340px',
+                  height: window.innerWidth > 420 ? '450px' : '400px',
+                  cursor: 'pointer',
+                  position: 'relative',
+                }}
+                onClick={() => addConfetti()}
+              >
+                {passportData?.nft?.image && (
+                  <Image src={passportData.nft.image} layout="fill" />
+                )}
+              </Card>
+            </div>
+            <div className="card shadow-md flex flex-row md:flex-col justify-center gap-4 p-4 bg-white h-fit">
+              <button
+                onClick={() => signMessageAndDownloadPass()}
+                className="w-40 md:w-full"
+              >
+                <Image src={AddToWallet} layout="responsive" />
+              </button>
+              <a
+                className="btn btn-primary gap-2"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://discord.gg/nation3"
+              >
+                <Image src={DiscordIcon} width={24} height={24} />
+                <span className="hidden md:block">Access gated channels</span>
+              </a>
+              <a
+                className="btn btn-primary gap-2"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://vote.nation3.org"
+              >
+                <Image src={BallotIcon} width={24} height={24} />
+                <span className="hidden md:block">Vote on proposals</span>
+              </a>
+            </div>
           </div>
-          <div className="flex flex-row md:flex-col justify-center items-between content-end mt-8 gap-4">
-            <button
-              onClick={() => signMessageAndDownloadPass()}
-              className="w-40 md:w-full"
-            >
-              <Image src={AddToWallet} layout="responsive" />
-            </button>
-            <a
-              className="btn btn-primary gap-2"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://discord.gg/nation3"
-            >
-              <Image src={DiscordIcon} width={24} height={24} />
-              <span className="hidden md:block">Access gated channels</span>
-            </a>
-            <a
-              className="btn btn-primary gap-2"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://vote.nation3.org"
-            >
-              <Image src={BallotIcon} width={24} height={24} />
-              <span className="hidden md:block">Vote on proposals</span>
-            </a>
-          </div>
-        </div>
+          <div className="card shadow-md p-4 bg-white mt-8 mx-4 lg:mx-32">
+            <div className="form-control w-full">
+              <h2 className="text-xl">Settings</h2>
+              <label className="label">
+                <span className="label-text">
+                  Signer account
+                  <br />
+                  <span className="text-xs">
+                    You can specify another account as a signer. This feature
+                    will be used in the future so you can participate in votes
+                    on the go, or authenticate yourself at in-person events
+                    without carrying your main private key.
+                  </span>
+                </span>
+              </label>
+              <input
+                type="text"
+                placeholder="Passport signer"
+                className="input input-bordered w-full"
+                value={passportSigner}
+                onChange={(e: any) => {
+                  setPassportSigner(e.target.value)
+                }}
+              />
 
-        <div className="divider"></div>
-
-        <div className="form-control w-full">
-          <h2 className="text-xl">Settings</h2>
-          <label className="label">
-            <span className="label-text">
-              Signer account
-              <br />
-              <span className="text-xs">
-                You can specify another account as a signer. This feature will
-                be used in the future so you can participate in votes on the go,
-                or authenticate yourself at in-person events without carrying
-                your main private key.
-              </span>
-            </span>
-          </label>
-          <input
-            type="text"
-            placeholder="Passport signer"
-            className="input input-bordered w-full"
-            value={passportSigner}
-            onChange={(e: any) => {
-              setPassportSigner(e.target.value)
-            }}
-          />
-
-          <div className="card-actions mt-4">
-            <ActionButton
-              className="btn btn-primary normal-case font-medium w-full"
-              action={updatePassportSigner}
-            >
-              Set passport signer
-            </ActionButton>
+              <div className="card-actions mt-4">
+                <ActionButton
+                  className="btn btn-primary normal-case font-medium w-full"
+                  action={updatePassportSigner}
+                >
+                  Set passport signer
+                </ActionButton>
+              </div>
+            </div>
           </div>
         </div>
-      </MainCard>
+      </div>
     </>
   )
 }
