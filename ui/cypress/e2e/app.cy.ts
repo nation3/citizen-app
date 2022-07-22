5dfe36ddb5fd2fc5e438ebdc51048c5b5d31fbad
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-07-22 11:41:59 +0800

    test: load pages

--- ui/cypress/e2e/app.cy.ts
+++ ui/cypress/e2e/app.cy.ts
@@ -1,18 +1,12 @@
-describe('Navigation', () => {
-  it('should navigate to the join page', () => {
-    // Start from the index page
+describe('Nation3 App UI', () => {
+
+  it('should load the index page', () => {
     cy.visit('/')
-
-    // The index page should contain an h1 with "Welcome to Nation3"
     cy.get('h1').contains('Welcome to Nation3')
+  }),
 
-    // Find a link with an href attribute containing "join" and click it
-    cy.get('a[href*="join"]').click()
-
-    // The new url should include "/join"
-    cy.url().should('include', '/join')
-
-    // The new page should contain an h2 with "Become a Nation3 citizen"
+  it('should load the /join page', () => {
+    cy.visit('/join')
     cy.get('h2').contains('Become a Nation3 citizen')
   })
 })
