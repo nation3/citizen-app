9514b3bf45f4a3274956845f8006d41e54671a05
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-07-22 11:48:48 +0800

    test: load /claim page

--- ui/cypress/e2e/app.cy.ts
+++ ui/cypress/e2e/app.cy.ts
@@ -8,6 +8,11 @@ describe('Nation3 App UI', () => {
   it('should load the /join page', () => {
     cy.visit('/join')
     cy.get('h2').contains('Become a Nation3 citizen')
+  }),
+
+  it('should load the /claim page', () => {
+    cy.visit('/claim')
+    cy.get('h2').contains('$NATION tweetdrop')
   })
 })
 
