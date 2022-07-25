44288e7d01fa118d24a40c865788b97976cf7177
Luis Iván Cuende <me@luisivan.net>
2022-07-25 11:17:44 +0100

    Update ui/lib/passport-nft.ts
    Co-authored-by: Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>

--- ui/lib/passport-nft.ts
+++ ui/lib/passport-nft.ts
@@ -64,7 +64,7 @@ export function usePassportSigner(id: number) {
   return useContractRead(
     {
       addressOrName: nationPassportNFT,
-      contractInterface: PassporNFT.abi,
+      contractInterface: PassportNFT.abi,
     },
     'signerOf',
     {
