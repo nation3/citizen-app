9223e5fc901f4cc5d0b271eaead0c63a109abb70
southbear <108498243+southbears@users.noreply.github.com>
2022-08-17 12:58:49 +0200

    bugfix: don't add errors indefinetly when wagmi hook fails

--- ui/lib/use-handle-error.ts
+++ ui/lib/use-handle-error.ts
@@ -1,13 +1,13 @@
-import { useEffect } from 'react'
-import { useErrorContext } from '../components/ErrorProvider'
+import { useEffect } from 'react';
+import { useErrorContext } from '../components/ErrorProvider';
 
 // For some contract interactions, a reverted call is not an error
 export function useHandleError(object: any, throwOnRevert = true) {
-  const errorContext = useErrorContext()
+  const {addError} = useErrorContext()
   useEffect(() => {
     if (throwOnRevert && object.error) {
-      errorContext.addError([object.error])
+      addError([object.error])
     }
-  }, [object.error, throwOnRevert, errorContext])
+  }, [object.error, throwOnRevert, addError])
   return object
 }
