be49b554ec33c0c8d62115d124ed0063f2a9c5d5
southbears <108498243+southbears@users.noreply.github.com>
2022-08-18 23:30:58 +0200

    Update ui/lib/use-handle-error.ts
    Co-authored-by: Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>

--- ui/lib/use-handle-error.ts
+++ ui/lib/use-handle-error.ts
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useErrorContext } from '../components/ErrorProvider';
+import { useEffect } from 'react'
+import { useErrorContext } from '../components/ErrorProvider'
 
 // For some contract interactions, a reverted call is not an error
 export function useHandleError(object: any, throwOnRevert = true) {
