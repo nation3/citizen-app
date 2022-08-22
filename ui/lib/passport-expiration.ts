e77be4c13ed1564c88f1d0b05da439d361b71dad
southbears <108498243+southbears@users.noreply.github.com>
2022-08-22 22:24:38 +0200

    Update ui/lib/passport-expiration.ts
    Co-authored-by: Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>

--- ui/lib/passport-expiration.ts
+++ ui/lib/passport-expiration.ts
@@ -15,7 +15,8 @@ export function usePassportExpirationDate(): Date | undefined {
       // threshold <= (lock amount) * (lock end - expiration) / (max lock period)
       // threshold * (max lock period) / (lock amount) <= (lock end - expiration)
       // expiration <= (lock end) - threshold * (max lock period) / (lock amount)
-      if (!veNationLock) return undefined;
+      if (!veNationLock)
+        return undefined;
       const [lockAmount, lockEnd]: [ethers.BigNumber, ethers.BigNumber] = veNationLock;
       const expiration = lockEnd.sub(threshold.mul(maxLockPeriod).div(lockAmount));
       return new Date(expiration.mul(1000).toNumber());
