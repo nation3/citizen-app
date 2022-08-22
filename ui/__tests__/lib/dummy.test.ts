ec1cba44fe96a787aa8031400baeafd98f60572c
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-22 12:03:21 +0800

    test: numbers.ts

--- ui/__tests__/lib/dummy.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from '@jest/globals'
-import { sum } from '../../lib/dummy' 
-
-test("Dummy unit test", () => {
-  const actual = sum(1, 2)
-  expect(actual).toBe(3)
-})
---
+++ ui/__tests__/lib/numbers.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from '@jest/globals'
+import { BigNumber } from 'ethers'
+import { NumberType, transformNumber } from '../../lib/numbers'
+
+test("transformNumber to Number", () => {
+  const actual = transformNumber(333, NumberType.number)
+  expect(actual).toBe(Number(333))
+})
+
+test("transformNumber to BigNumber", () => {
+  const actual = transformNumber(333, NumberType.bignumber)
+  expect(actual).toBeInstanceOf(BigNumber)
+})
+
+test("transformNumber to String", () => {
+  const actual = transformNumber(333, NumberType.string)
+  expect(actual).toBe("333.000000000000000000")
+})
+
+test("transformNumber to String (2 decimals)", () => {
+  const actual = transformNumber(333, NumberType.string, 2)
+  expect(actual).toBe("333.00")
+})
