333486706eb2f266251b286d89a4783f9b993fd4
southbear <108498243+southbears@users.noreply.github.com>
2022-09-04 21:22:40 +0200

    bugfix: don't crash if current account has 0 veNation

--- ui/lib/passport-expiration.ts
+++ ui/lib/passport-expiration.ts
@@ -5,7 +5,9 @@ import { useVeNationLock } from "./ve-token";
 
 const decimalThreshold = Number(process.env.NEXT_PUBLIC_CITIZENSHIP_REVOKE_THRESHOLD)
 const threshold = ethers.BigNumber.from(String(decimalThreshold * 10 ** 18));
-const maxLockPeriod = ethers.BigNumber.from(31556926 * 4);
+
+const fourYearsInSeconds = 31556926 * 4
+const maxLockPeriod = ethers.BigNumber.from(fourYearsInSeconds);
 
 export function usePassportExpirationDate(): Date | undefined {
   const { data: account } = useAccount()
@@ -18,6 +20,7 @@ export function usePassportExpirationDate(): Date | undefined {
       // expiration <= (lock end) - threshold * (max lock period) / (lock amount)
       if (!veNationLock) return undefined;
       const [lockAmount, lockEnd]: [ethers.BigNumber, ethers.BigNumber] = veNationLock;
+      if (lockAmount.isZero()) return undefined;
       const expiration = lockEnd.sub(threshold.mul(maxLockPeriod).div(lockAmount));
       return new Date(expiration.mul(1000).toNumber());
   }, [veNationLock]);
