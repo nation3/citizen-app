bcbcbec5a151c633b8ce9401973a9f5a44dffb5c
TTNguyenDev <nttriet9a4@gmail.com>
2024-01-20 00:12:43 +0700

    remove nationPassportRevokeUnderBalance from config

--- ui/lib/passport-expiration-hook.ts
+++ ui/lib/passport-expiration-hook.ts
@@ -3,20 +3,28 @@ import { useMemo } from "react";
 import { getPassportExpirationDate } from "./passport-expiration";
 import { useAccount } from "./use-wagmi";
 import { useVeNationLock } from "./ve-token";
-import { nationPassportRevokeUnderBalance } from "../lib/config";
-
+import {
+  useContractRead,
+} from './use-wagmi'
+import { nationPassportNFTIssuer } from '../lib/config'
+import PassportIssuer from '../abis/PassportIssuer.json'
 export function usePassportExpirationDate(): Date | undefined {
   const { address } = useAccount()
   const { data: veNationLock } = useVeNationLock(address)
 
-  const threshold = ethers.BigNumber.from(String(nationPassportRevokeUnderBalance * 10 ** 18));
-    
-  return useMemo(() => {
-      if (!veNationLock) {
-        return undefined;
-      }
+  const contractParams = {
+    address: nationPassportNFTIssuer,
+    abi: PassportIssuer.abi,
+  }
+  const { data: revokeUnderBalance } =
+    useContractRead(contractParams, 'revokeUnderBalance')
 
-      const [lockAmount, lockEnd]: [ethers.BigNumber, ethers.BigNumber] = veNationLock;
-      return getPassportExpirationDate(lockAmount, lockEnd, threshold);
-  }, [veNationLock, threshold]);
+  return useMemo(() => {
+    if (!veNationLock || !revokeUnderBalance) {
+      return undefined;
+    }
+
+    const [lockAmount, lockEnd]: [ethers.BigNumber, ethers.BigNumber] = veNationLock;
+    return getPassportExpirationDate(lockAmount, lockEnd, revokeUnderBalance);
+  }, [veNationLock, revokeUnderBalance]);
 }
