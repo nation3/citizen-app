1bbb1c9198652c0f93b5a96f4f45a54d6f5a7061
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-01-20 05:22:55 +0100

    fix: replace external function for hooks

--- ui/lib/passport-expiration-hook.ts
+++ ui/lib/passport-expiration-hook.ts
@@ -1,30 +1,32 @@
 import { ethers } from 'ethers'
 import { useMemo } from 'react'
+import PassportIssuer from '../abis/PassportIssuer.json'
 import { nationPassportNFTIssuer } from './config'
-import { getRevokeUnderBalance } from './get-revoke-under-balance'
 import { getPassportExpirationDate } from './passport-expiration'
-import { useAccount } from './use-wagmi'
+import { useAccount, useContractRead } from './use-wagmi'
 import { useVeNationLock } from './ve-token'
 
 export function usePassportExpirationDate(): Date | undefined {
   const { address } = useAccount()
+
   const { data: veNationLock } = useVeNationLock(address)
 
-  let threshold: any
-
-  getRevokeUnderBalance(nationPassportNFTIssuer)
-    .then((res) => {
-      threshold = ethers.BigNumber.from(res)
-    })
-    .catch((err) => console.error(err.message))
+  const { data: threshold, error } = useContractRead(
+    {
+      address: nationPassportNFTIssuer,
+      abi: PassportIssuer.abi,
+    },
+    'revokeUnderBalance'
+  )
 
   return useMemo(() => {
     if (!veNationLock) {
       return undefined
     }
 
-    const [lockAmount, lockEnd]: [ethers.BigNumber, ethers.BigNumber] = veNationLock
+    const [lockAmount, lockEnd]: [ethers.BigNumber, ethers.BigNumber] =
+      veNationLock
 
-    return getPassportExpirationDate(lockAmount, lockEnd, threshold)
+    return getPassportExpirationDate(lockAmount, lockEnd, threshold!)
   }, [veNationLock, threshold])
 }
