fb5ef8fcadfd7f547e33182278c695b9b055acc0
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-08-22 13:25:14 +0700

    build: nextjs v13 link component

--- ui/components/GradientLink.tsx
+++ ui/components/GradientLink.tsx
@@ -2,26 +2,9 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function GradientLink({ text, href, textSize }: any) {
-  if (href?.charAt(0) === '/') {
-    return (
-      <Link href={href}>
-        <a
-          className={`text-${textSize} font-medium bg-gradient-to-r from-n3blue to-n3green text-transparent bg-clip-text`}
-        >
-          {text} →
-        </a>
-      </Link>
-    )
-  } else {
-    return (
-      <a
-        className={`text-${textSize} font-medium bg-gradient-to-r from-n3blue to-n3green text-transparent bg-clip-text`}
-        href={href}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
+  return (
+    <Link href={href} className={`text-${textSize} font-medium bg-gradient-to-r from-n3blue to-n3green text-transparent bg-clip-text`}>
         {text} →
-      </a>
-    )
-  }
+    </Link>
+  )
 }
